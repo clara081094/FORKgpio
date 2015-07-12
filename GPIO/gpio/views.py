53b0f6f5cfc8180c251799526c2e4bacbe34171c
Clara <clara.081094@gmail.com>
2015-07-12 11:53:57 -0500

    Aplicacion terminada

--- GPIO/gpio/views.py
+++ GPIO/gpio/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
+from gpio.models import Nombre
+from django.contrib import messages
 #import RPi.GPIO as GPIO
 import time
 from random import randint
@@ -17,7 +19,7 @@ def login_user(request):
         return HttpResponseRedirect('/gpio/')
 
     logout(request)
-    state = "Registrese en la parte superior"
+    state = "Ingrese su usuario y password"
     username = password = ''
     if request.POST:
         username = request.POST['username']
@@ -26,6 +28,7 @@ def login_user(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
+                request.session.set_expiry(60)
                 login(request, user)
                 return HttpResponseRedirect('/gpio/')
             else:
@@ -39,8 +42,8 @@ def login_user(request):
 @csrf_exempt
 @login_required(login_url='/login/')
 def index(request):
-	#estados=GPIO.input(23)
-    #estado+
+    if 'action3' in request.POST.keys():
+        return HttpResponseRedirect('/conf/')
     if 'action1' in request.POST.keys():
        encender()
     if 'action2' in request.POST.keys():
@@ -53,9 +56,49 @@ def index(request):
     #GPIO.setup(16, GPIO.IN)
     #estado = GPIO.input(16) # 0 --> prendido #ACA LEEE
     estado=0
+    planta=Nombre.objects.get(pk=1)
     return render_to_response('gpio/indexCalc.html',
-    	{'estado':estado,}
+    	{'estado':estado,'planta':planta}
      )
+
+@csrf_exempt
+@login_required(login_url='/login/')
+def conf_user(request):
+
+    state="Marque las opciones que desea cambiar"
+
+    if request.POST:
+        Nplanta = request.POST['Nplanta']
+        Apassword = request.POST['Apassword']
+        Ppassword = request.POST['Ppassword']
+        try:
+            option=request.POST['vehicle']
+        except Exception,e:
+            option="op"
+
+        if option=="op":
+            return HttpResponseRedirect('/gpio/')
+
+        us = authenticate(username=request.user.username, password=Apassword)
+        if option=="nom":
+            if us is not None:
+                plant=Nombre.objects.get(pk=1)
+                plant.nombre_text = Nplanta
+                plant.save()
+                return HttpResponseRedirect('/gpio/')     
+            else:
+                state = "Su password es incorrecto."
+        if option=="pass":
+            if us is not None :
+                request.user.set_password(Ppassword)
+                request.user.save()
+                state="Password cambiado correctamente, loguese para ingresar"
+                return render_to_response('gpio/login.html',{'state':state})
+            else:
+                state = "Su password es incorrecto."      
+
+    planta=Nombre.objects.get(pk=1)
+    return render_to_response('gpio/conf.html',{'state': state,'planta': planta})
 
 def encender():
     print("encendiendo")
